b246afe55689585dbe7da61b48b3191b9f744683
Stefan Toma <stefan@Stefans-MBP.mbrv>
2024-07-15 23:00:42 +0300

    PUT DELETE methods

--- rest_app.py
+++ rest_app.py
@@ -7,49 +7,70 @@ app = Flask(__name__)
 
 @app.route('/users/<user_id>', methods=['GET'])
 def get_user_data(user_id):
-    if request.method == 'GET':
-        try:
-            user_data = db_connector.get_user_data(user_id)
-            if user_data:
-                return jsonify({
-                    "status": "ok",
-                    "user_name": user_data[0][1]
-                }), 200
-            else:
-                return jsonify({
-                    "status": "error",
-                    "reason": "no such id"
-                }), 500
-        except pymysql.Error as error:
-            print(error)
+    try:
+        user_data = db_connector.get_user_data(user_id)
+        if user_data:
+            return jsonify({
+                "status": "ok",
+                "user_name": user_data[0][1]
+            }), 200
+        else:
+            return jsonify({
+                "status": "error",
+                "reason": "no such id"
+            }), 500
+    except pymysql.Error as error:
+        print(error)
 
 
 @app.route('/users/<user_id>', methods=['POST'])
 def create_user(user_id):
-    if request.method == 'POST':
-        request_data = request.json
-        user_name = request_data.get('user_name')
-        try:
-            db_connector.create_user(user_id=user_id, user_name=user_name)
-            return jsonify({
-                "status": "ok",
-                "user_name": user_name
-            }), 200
-        except pymysql.IntegrityError:
-            return jsonify({
-                "status": "error",
-                "reason": "id already exists"
-            }), 500
+    request_data = request.json
+    user_name = request_data.get('user_name')
+    try:
+        db_connector.create_user(user_id=user_id, user_name=user_name)
+        return jsonify({
+            "status": "ok",
+            "user_name": user_name
+        }), 200
+    except pymysql.IntegrityError:
+        return jsonify({
+            "status": "error",
+            "reason": "id already exists"
+        }), 500
 
 
 @app.route('/users/<user_id>', methods=['PUT'])
 def modify_user(user_id):
-    pass
+    request_data = request.json
+    user_name = request_data.get('user_name')
+    if user_name:
+        try:
+            db_connector.modify_user_data(user_id, user_name)
+            return jsonify({
+                "status": "ok",
+                "user_updated": user_name
+            }), 200
+        except pymysql.Error:
+            return jsonify({
+                "status": "error",
+                "reason": "no such id"
+            }), 500
 
 
 @app.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
-    pass
+    try:
+        db_connector.remove_user(user_id)
+        return jsonify({
+            "status": "ok",
+            "user_deleted": user_id
+        }), 200
+    except pymysql.Error:
+        return jsonify({
+            "status": "error",
+            "reason": "no such id"
+        }), 500
 
 
 app.run(host='127.0.0.1', port=5600)
