837b6f9244e02f1c2247b3746493f9bead37c875
Stefan Toma <stefan@Stefans-MBP.mbrv>
2024-07-16 23:35:16 +0300

    rest app docstrings

--- rest_app.py
+++ rest_app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 
 @app.route('/users/<user_id>', methods=['GET'])
 def get_user_data(user_id):
+    """
+    Retrieves the user data using its ID
+    If the ID does not exist it will return "no such id"
+    If there is a mysql error it will get printed to the console
+    :param user_id:
+    :return:
+    """
     try:
         user_data = db_connector.get_user_data(user_id)
         if user_data:
@@ -25,6 +32,12 @@ def get_user_data(user_id):
 
 @app.route('/users/<user_id>', methods=['POST'])
 def create_user(user_id):
+    """
+    Accepts a json containing the name of the user to be created
+    If the ID exists it will return an error: "id already exists"
+    :param user_id:
+    :return:
+    """
     request_data = request.json
     user_name = request_data.get('user_name')
     try:
@@ -42,6 +55,11 @@ def create_user(user_id):
 
 @app.route('/users/<user_id>', methods=['PUT'])
 def modify_user(user_id):
+    """
+    Accepts the new username and the ID of the user that needs to be modified
+    :param user_id:
+    :return:
+    """
     request_data = request.json
     user_name = request_data.get('user_name')
     if user_name:
@@ -60,6 +78,11 @@ def modify_user(user_id):
 
 @app.route('/users/<user_id>', methods=['DELETE'])
 def delete_user(user_id):
+    """
+    Deletes the user using the user ID
+    :param user_id:
+    :return:
+    """
     try:
         db_connector.remove_user(user_id)
         return jsonify({
@@ -73,4 +96,5 @@ def delete_user(user_id):
         }), 500
 
 
-app.run(host='127.0.0.1', port=5600)
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5600)
