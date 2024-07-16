81c461ddf0e8b07139e391371d77ea137955ff16
Stefan Toma <stefan@Stefans-MBP.mbrv>
2024-07-16 21:43:22 +0300

    initial frontend testing

---
+++ frontend_testing.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=Service(""))
+
+driver.get('http://127.0.0.1:5001/users/get_user_data/2')
+user_element = driver.find_element(By.ID, value='user')
+if user_element:
+    print(user_element.text)
